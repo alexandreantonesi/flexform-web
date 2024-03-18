14fb35551bb3b25b6ba4e24a08341e95ea3a3e83
Alexandre Antonesi <ti212188@ensiguarda.com>
2024-03-18 10:54:50 +0000

    aa

--- src/components/PaginaExercicios.js
+++ src/components/PaginaExercicios.js
@@ -1,90 +1,122 @@
 import React, { useState, useEffect } from 'react';
 import { ListGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 import { format } from 'date-fns';
-import pt from 'date-fns/locale/pt';
+import { pt } from 'date-fns/locale';
 import ItemExercicio from './ItemExercicio';
+import axios from 'axios';
 
-const diasDisponiveis = 5;
+const PaginaExercicios = () => {
+  const [diasDisponiveis, setDiasDisponiveis] = useState(0);
+  const [exerciciosHoje, setExerciciosHoje] = useState([]);
+  const [diaSelecionado, setDiaSelecionado] = useState('');
 
-const exercisesByDay = {
-  push: [
-    { name: 'Supino com Halteres', muscles: 'Cabeça esternocostal do peitoral maior, tríceps, deltoides anteriores' },
-    { name: 'Peck Deck', muscles: 'Cabeça esternocostal do peitoral maior, tríceps, deltoides anteriores' },
-    { name: 'Elevação Frontal com Cabo', muscles: 'Cabeça clavicular do peitoral maior, deltoides anteriores' },
-    { name: 'Supino Inclinado com Halteres', muscles: 'Cabeça clavicular do peitoral maior, tríceps, deltoides anteriores' }
-  ],
-  pull: [
-    { name: 'Rosca com Cabo', muscles: 'Bíceps, braquial' },
-    { name: 'Rosca Direta', muscles: 'Bíceps, braquial' },
-    { name: 'Rosca Scott', muscles: 'Cabeça curta do bíceps, braquial' },
-    { name: 'Rosca Inclinada', muscles: 'Cabeça longa do bíceps, braquial' },
-    { name: 'Remada em T', muscles: 'Músculos das costas' },
-    { name: 'Pulldown', muscles: 'Músculos das costas' },
-    { name: 'Remada Sentada', muscles: 'Músculos das costas' },
-    { name: 'Voador Inverso', muscles: 'Deltoides posteriores' }
-  ],
-  legs: [
-    { name: 'Agachamento', muscles: 'Músculos das pernas' },
-    { name: 'Prensa de Pernas', muscles: 'Músculos das pernas' },
-    { name: 'Extensão de Pernas', muscles: 'Músculos das pernas' },
-    { name: 'Flexão de Pernas', muscles: 'Músculos das pernas' }
-  ],
-  rest: []
-};
+  useEffect(() => {
+    const fetchDiasDisponiveis = async () => {
+      try {
+        const { data } = await axios.get('/caminho/para/servidor/api/diasDisponiveis');
+        setDiasDisponiveis(data.dias_disponiveis);
+        const today = format(new Date(), 'EEEE', { locale: pt });
+        setDiaSelecionado(today);
+        setExerciciosHoje(getTodaysExercises(today, data.dias_disponiveis));
+      } catch (error) {
+        console.error('Erro ao obter os dias disponíveis: ', error);
+      }
+    };
 
-const dayMap = {
-  'Monday': 'segunda-feira',
-  'Tuesday': 'terça-feira',
-  'Wednesday': 'quarta-feira',
-  'Thursday': 'quinta-feira',
-  'Friday': 'sexta-feira',
-  'Saturday': 'sábado',
-  'Sunday': 'domingo',
-};
-
-const getTodaysExercises = (day, diasDisponiveis) => {
-  let exercisePlan = {};
-  switch(diasDisponiveis) {
-    // Define exercise plan based on diasDisponiveis
-    // ...
-  }
-  return exercisesByDay[exercisePlan[day]] || [];
-};
-
-const ExercisePage = () => {
-  const [todaysExercises, setTodaysExercises] = useState([]);
-  const [selectedDay, setSelectedDay] = useState('');
+    fetchDiasDisponiveis();
+  }, []);
 
   useEffect(() => {
     const todayFormatted = format(new Date(), 'EEEE', { locale: pt });
-    const dayInEnglish = Object.keys(dayMap).find(key => dayMap[key] === todayFormatted);
-    setSelectedDay(dayMap[todayFormatted]);
-    setTodaysExercises(getTodaysExercises(dayInEnglish, diasDisponiveis));
-  }, []);
+    setExerciciosHoje(getTodaysExercises(todayFormatted, diasDisponiveis));
+  }, [diasDisponiveis, diaSelecionado]);
 
-  const handleDaySelect = (eventKey, event) => {
-    const day = dayMap[eventKey];
-    setSelectedDay(day);
-    setTodaysExercises(getTodaysExercises(eventKey, diasDisponiveis));
+  const handleDaySelect = (eventKey) => {
+    const selectedDayPortuguese = dayMap[eventKey];
+    setDiaSelecionado(selectedDayPortuguese);
+    setExerciciosHoje(getTodaysExercises(eventKey, diasDisponiveis));
+  };
+
+  const exercisesByDay = {
+    push: [
+      { nome: 'Supino com Halteres', musculos: 'Cabeça esternocostal do peitoral maior, tríceps, deltoides anteriores' },
+      { nome: 'Peck Deck', musculos: 'Cabeça esternocostal do peitoral maior, tríceps, deltoides anteriores' },
+      { nome: 'Elevação Frontal com Cabo', musculos: 'Cabeça clavicular do peitoral maior, deltoides anteriores' },
+      { nome: 'Supino Inclinado com Halteres', musculos: 'Cabeça clavicular do peitoral maior, tríceps, deltoides anteriores' }
+    ],
+    pull: [
+      { nome: 'Rosca com Cabo', musculos: 'Bíceps, braquial' },
+      { nome: 'Rosca Direta', musculos: 'Bíceps, braquial' },
+      { nome: 'Rosca Scott', musculos: 'Cabeça curta do bíceps, braquial' },
+      { nome: 'Rosca Inclinada', musculos: 'Cabeça longa do bíceps, braquial' },
+      { nome: 'Remada em T', musculos: 'Músculos das costas' },
+      { nome: 'Pulldown', musculos: 'Músculos das costas' },
+      { nome: 'Remada Sentada', musculos: 'Músculos das costas' },
+      { nome: 'Voador Inverso', musculos: 'Deltoides posteriores' }
+    ],
+    legs: [
+      { nome: 'Agachamento', musculos: 'Músculos das pernas' },
+      { nome: 'Prensa de Pernas', musculos: 'Músculos das pernas' },
+      { nome: 'Extensão de Pernas', musculos: 'Músculos das pernas' },
+      { nome: 'Flexão de Pernas', musculos: 'Músculos das pernas' }
+    ],
+    rest: []
+  };
+
+  const dayMap = {
+    'Monday': 'segunda-feira',
+    'Tuesday': 'terça-feira',
+    'Wednesday': 'quarta-feira',
+    'Thursday': 'quinta-feira',
+    'Friday': 'sexta-feira',
+    'Saturday': 'sábado',
+    'Sunday': 'domingo',
+  };
+
+  const getTodaysExercises = (day, availableDays) => {
+    const dayInEnglish = Object.keys(dayMap).find(key => dayMap[key] === day);
+    let exercisePlan = {};
+    if (availableDays === 5) {
+      exercisePlan = {
+        'Monday': 'push', 'Tuesday': 'pull', 'Wednesday': 'rest', 'Thursday': 'legs',
+        'Friday': 'push', 'Saturday': 'pull', 'Sunday': 'rest'
+      };
+    } else if (availableDays === 4) {
+      exercisePlan = {
+        'Monday': 'push', 'Tuesday': 'pull', 'Wednesday': 'rest', 'Thursday': 'push',
+        'Friday': 'pull', 'Saturday': 'rest', 'Sunday': 'rest'
+      };
+    } else if (availableDays === 3) {
+      exercisePlan = {
+        'Monday': 'push', 'Tuesday': 'pull', 'Wednesday': 'rest', 'Thursday': 'legs',
+        'Friday': 'rest', 'Saturday': 'rest', 'Sunday': 'rest'
+      };
+    } else if (availableDays === 2) {
+      exercisePlan = {
+        'Monday': 'push', 'Tuesday': 'rest', 'Wednesday': 'rest', 'Thursday': 'pull',
+        'Friday': 'rest', 'Saturday': 'rest', 'Sunday': 'rest'
+      };
+    } else {
+      return [];
+    }
+    return exercisesByDay[exercisePlan[dayInEnglish]] || [];
   };
 
   return (
     <div className='m-3'>
-      <DropdownButton id='day-select-dropdown' title={selectedDay || 'Selecionar Dia'} onSelect={handleDaySelect}>
-        {Object.values(dayMap).map((dayName, index) => (
-          <Dropdown.Item key={dayName} eventKey={Object.keys(dayMap)[index]}>{dayName}</Dropdown.Item>
+      <DropdownButton id='day-select-dropdown' title={diaSelecionado || 'Selecionar Dia'} onSelect={handleDaySelect}>
+        {Object.keys(dayMap).map((dayKey) => (
+          <Dropdown.Item key={dayKey} eventKey={dayKey}>{dayMap[dayKey]}</Dropdown.Item>
         ))}
       </DropdownButton>
-      <h2>Exercícios para hoje - {selectedDay || 'Hoje'}</h2>
+      <h2>Exercícios para hoje - {diaSelecionado || 'Hoje'}</h2>
       <ListGroup>
-        {todaysExercises.length > 0 ? todaysExercises.map((exercicio, index) => (
+        {exerciciosHoje.length > 0 ? exerciciosHoje.map((exercicio, index) => (
           <ItemExercicio key={index} exercicio={exercicio} />
-        )) : (
-          <p>Nenhum exercício planejado para hoje.</p>
-        )}
+        )) : <p>Nenhum exercício planejado para hoje.</p>}
       </ListGroup>
     </div>
   );
 };
 
-export default ExercisePage;
+export default PaginaExercicios;
